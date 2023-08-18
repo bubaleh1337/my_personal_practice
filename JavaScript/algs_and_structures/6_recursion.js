7009f05dee9a818a14a43eef0da93ca9490149ed
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-08-18 13:45:05 +0500

    initital commit for JS

---
+++ JavaScript/algs_and_structures/6_recursion.js
@@ -0,0 +1,13 @@
+const factorial = (n) => {
+	if (n === 1) return 1
+	return n * factorial(n - 1)
+}
+console.log("factorial: " + factorial(5))
+
+// Числа Фибоначчи - 1, 1, 2, 3, 5, 8, 13, 21
+
+const fibonachi = (n) => {
+	if(n === 1 || n === 2) return 1
+	return fibonachi(n - 1) + fibonachi(n - 2)
+}
+console.log("fibonachi: " + fibonachi(8)) 
